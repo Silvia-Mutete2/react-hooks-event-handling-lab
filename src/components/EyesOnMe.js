40bc4b8d6f114a283bd12765a08357abe7d888d1
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-07-13 16:26:22 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+ function EyesOnMe(){
+    return (
+        <div>
+        <React.Fragment>
+            <button onBlur={() => console.log('Hey! Eyes on me!')} 
+                     onFocus={() => console.log('Good!')}>
+                         Eyes on me
+            </button>
+        </React.Fragment>
+        </div>
+    )
+}
+
+ export default EyesOnMe
